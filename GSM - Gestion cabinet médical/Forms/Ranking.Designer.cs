eddec06e66279f2c82db27e94a8af2ff18a19a26
AyoubAitOuhamd(Aka MRX) <ayoubaithmad77@gmail.com>
2025-04-25 13:22:33 +0100

    add ranking form with possibility ta handle to types of ranking

---
+++ GSM - Gestion cabinet médical/Forms/Ranking.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace GSM___Gestion_cabinet_médical.Forms
+{
+    partial class Ranking
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.RankingTitle = new System.Windows.Forms.Label();
+            this.RankingTypeOneList = new System.Windows.Forms.ListView();
+            this.RankingTypeOneTitle = new System.Windows.Forms.Label();
+            this.RankingTypeTwoTitle = new System.Windows.Forms.Label();
+            this.RankingTypeTwoList = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // RankingTitle
+            // 
+            this.RankingTitle.AutoSize = true;
+            this.RankingTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RankingTitle.Location = new System.Drawing.Point(30, 28);
+            this.RankingTitle.Name = "RankingTitle";
+            this.RankingTitle.Size = new System.Drawing.Size(64, 25);
+            this.RankingTitle.TabIndex = 0;
+            this.RankingTitle.Text = "label1";
+            // 
+            // RankingTypeOneList
+            // 
+            this.RankingTypeOneList.HideSelection = false;
+            this.RankingTypeOneList.Location = new System.Drawing.Point(33, 104);
+            this.RankingTypeOneList.Name = "RankingTypeOneList";
+            this.RankingTypeOneList.Size = new System.Drawing.Size(304, 317);
+            this.RankingTypeOneList.TabIndex = 1;
+            this.RankingTypeOneList.UseCompatibleStateImageBehavior = false;
+            // 
+            // RankingTypeOneTitle
+            // 
+            this.RankingTypeOneTitle.AutoSize = true;
+            this.RankingTypeOneTitle.Location = new System.Drawing.Point(30, 88);
+            this.RankingTypeOneTitle.Name = "RankingTypeOneTitle";
+            this.RankingTypeOneTitle.Size = new System.Drawing.Size(35, 13);
+            this.RankingTypeOneTitle.TabIndex = 3;
+            this.RankingTypeOneTitle.Text = "label1";
+            // 
+            // RankingTypeTwoTitle
+            // 
+            this.RankingTypeTwoTitle.AutoSize = true;
+            this.RankingTypeTwoTitle.Location = new System.Drawing.Point(465, 88);
+            this.RankingTypeTwoTitle.Name = "RankingTypeTwoTitle";
+            this.RankingTypeTwoTitle.Size = new System.Drawing.Size(35, 13);
+            this.RankingTypeTwoTitle.TabIndex = 4;
+            this.RankingTypeTwoTitle.Text = "label2";
+            // 
+            // RankingTypeTwoList
+            // 
+            this.RankingTypeTwoList.HideSelection = false;
+            this.RankingTypeTwoList.Location = new System.Drawing.Point(468, 104);
+            this.RankingTypeTwoList.Name = "RankingTypeTwoList";
+            this.RankingTypeTwoList.Size = new System.Drawing.Size(304, 317);
+            this.RankingTypeTwoList.TabIndex = 2;
+            this.RankingTypeTwoList.UseCompatibleStateImageBehavior = false;
+            // 
+            // Ranking
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(829, 450);
+            this.Controls.Add(this.RankingTypeTwoTitle);
+            this.Controls.Add(this.RankingTypeOneTitle);
+            this.Controls.Add(this.RankingTypeTwoList);
+            this.Controls.Add(this.RankingTypeOneList);
+            this.Controls.Add(this.RankingTitle);
+            this.Name = "Ranking";
+            this.Text = "Ranking";
+            this.Load += new System.EventHandler(this.Ranking_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label RankingTitle;
+        private System.Windows.Forms.ListView RankingTypeOneList;
+        private System.Windows.Forms.Label RankingTypeOneTitle;
+        private System.Windows.Forms.Label RankingTypeTwoTitle;
+        private System.Windows.Forms.ListView RankingTypeTwoList;
+    }
+}
---
+++ GSM - Gestion cabinet médical/Forms/Ranking.cs
@@ -0,0 +1,152 @@
+﻿using GSM___Gestion_cabinet_médical.Enums;
+using GSM___Gestion_cabinet_médical.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using ListView = System.Windows.Forms.ListView;
+
+namespace GSM___Gestion_cabinet_médical.Forms
+{
+    public partial class Ranking : Form
+    {
+        private int FormRole;
+        public static int FormRankingByAge = 0;
+        public static int FormRankingByGender = 1 ;
+
+        /*public static String RankingTypeOneList;
+        public static String RankingTypeTwoList;*/
+
+
+
+        public Ranking(int formRole)
+        {
+            InitializeComponent();
+            FormRole = formRole;
+        }
+
+        private void Ranking_Load(object sender, EventArgs e)
+        {
+            switch (FormRole)
+            {
+                case var value when value == Ranking.FormRankingByAge:
+
+                    LoadFormForRankingByAge();
+                    break;
+                case var value when value == Ranking.FormRankingByGender:
+                    LoadFormForRankingByGender();
+                    break;
+            }
+        }
+
+        //
+        public void LoadFormForRankingByGender()
+        {
+            // 
+            RankingTitle.Text = "Classer par sexe";
+            RankingTypeOneTitle.Text = Gender.Femme.ToString();
+            RankingTypeTwoTitle.Text = Gender.Homme.ToString();
+            //
+            ListView FemmalesPatientsListView = RankingTypeOneList;
+            ListView MensPatientsListView = RankingTypeTwoList;
+
+            InitializeListViewByGender(FemmalesPatientsListView);
+            InitializeListViewByGender(MensPatientsListView);
+
+
+            foreach (var patientRecord in Cabinet.Patients)
+            {
+                if(patientRecord.Sexe == Gender.Femme)
+                    FemmalesPatientsListView.Items.Add(FillListViewItemByGender(patientRecord));
+                else
+                    MensPatientsListView.Items.Add(FillListViewItemByGender(patientRecord));
+                
+            }
+
+
+        }
+        private ListViewItem FillListViewItemByGender(Patient patientRecord)
+        {
+            ListViewItem item = new ListViewItem(patientRecord.Nom);
+            item.SubItems.Add(patientRecord.Prenom);
+            item.SubItems.Add(patientRecord.DateNaissance.ToString());
+            item.Tag = patientRecord.Id;  
+            return item;
+       
+        }
+        private void InitializeListViewByGender(ListView listView)
+        {
+
+            listView.View = View.Details;
+            listView.FullRowSelect = true;
+            listView.GridLines = true;
+            listView.Columns.Add("Nom", 100);
+            listView.Columns.Add("Prenom", 100);
+            listView.Columns.Add("Date de naissance,", 100);
+
+            listView.View = View.Details;
+            listView.FullRowSelect = true;
+        }
+
+        //
+        public void LoadFormForRankingByAge()
+        {
+            RankingTitle.Text = "Classer par âge";
+            RankingTypeOneTitle.Text = "Adulte";
+            RankingTypeTwoTitle.Text = "Enfant";
+
+            ListView AdulesPatientsListView = RankingTypeOneList;
+            ListView KidsPatientsListView = RankingTypeTwoList;
+            InitializeListViewByAge(AdulesPatientsListView);
+            InitializeListViewByAge(KidsPatientsListView);
+
+
+            foreach (var patientRecord in Cabinet.Patients)
+            {
+                if (patientRecord.Age() >= 18)
+                    AdulesPatientsListView.Items.Add(FillListViewItemByAge(patientRecord));
+                else
+                    KidsPatientsListView.Items.Add(FillListViewItemByAge(patientRecord));
+
+            }
+
+        }
+
+        private ListViewItem FillListViewItemByAge(Patient patientRecord)
+        {
+            ListViewItem item = new ListViewItem(patientRecord.Nom);
+            item.SubItems.Add(patientRecord.Prenom);
+            item.SubItems.Add(patientRecord.Age().ToString());
+            item.Tag = patientRecord.Id;
+            return item;
+
+        }
+
+
+
+
+
+
+        private void InitializeListViewByAge(ListView listView)
+        {
+
+            listView.View = View.Details;
+            listView.FullRowSelect = true;
+            listView.GridLines = true;
+            listView.Columns.Add("Nom", 100);
+            listView.Columns.Add("Prenom", 100);
+            listView.Columns.Add("Age", 100);
+            listView.Columns.Add("Sexe", 100);
+           
+
+            listView.View = View.Details;
+            listView.FullRowSelect = true;
+        }
+    }
+}
